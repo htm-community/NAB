4a4b28c526e2be00a11de0c75be1156a274cf994
Marek Otahal <markotahal@gmail.com>
2019-07-10 01:40:03 +0200

    autoinstall Htmjava detector

--- install_py2_detectors.sh
+++ install_py2_detectors.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # This is a helper script to install python2 detectors (numenta, htmjava) if possible.
-# It is done through creating a python2 virtualenv and installing the packages there. 
+# It is done through creating a python2 virtualenv and installing the packages there.
+# Requirements: virtualenv, pytnon2, bash
+# If fails, nothing happens, but py2 detectors won't be usable
 
 [ ! -x "`which virtualenv`" ] && echo "Virtualenv required!" && exit
 [ ! -x "`which python2`" ] && echo "Python 2 not installed, skipping installation of python2-only detectors (numenta, numentaTM, htmjava)" && exit
@@ -21,6 +23,15 @@ python --version
 pip install nupic --force
 python nab/detectors/numenta/setup.py develop 
 
-pip list
+# 2/ htmjava detector
 
-deactivate
+[ ! -x "`which gradle`" ] && echo "htmjava detector requires gradle to build" && exit
+[ ! -x "`which java`" ] && "htmjava requires java! :) Preferably Java 8" && exit
+
+cd ./nab/detectors/htmjava/nab/detectors/htmjava #inception, I know :P 
+gradle clean build || exit
+cd ${REPO}
+python nab/detectors/htmjava/setup.py develop
+
+echo "Installation of Py2 detectors finished:"
+pip list | grep nab
